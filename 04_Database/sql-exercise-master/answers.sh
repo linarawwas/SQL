b267424339987c1fd1222659809af7ff0804b669
linarawwas <linamkraw2@gmail.com>
2022-12-11 23:47:49 +0100

    Creating Table

--- 04_Database/sql-exercise-master/answers.sh
+++ 04_Database/sql-exercise-master/answers.sh
@@ -1,17 +1,40 @@
-SELECT * FROM students;
-SELECT * FROM students
-WHERE Age > 30;
-SELECT * FROM students
-where age=30 AND Gender='F';
-SELECT Points FROM students
-WHERE Name='Alex';
-INSERT INTO students
-VALUES (7,'Lina',23,'F',900);
-UPDATE students
+"SELECT * FROM students;"
+"SELECT * FROM students
+WHERE Age > 30;"
+"SELECT * FROM students
+where age=30 AND Gender='F';"
+"SELECT Points FROM students
+WHERE Name='Alex';"
+"INSERT INTO students
+VALUES (7,'Lina',23,'F',900);"
+"UPDATE students
 SET Points=500
-WHERE Name='Basma';
-UPDATE students
+WHERE Name='Basma';"
+"UPDATE students
 set Points=100
-WHERE Name='Alex';
+WHERE Name='Alex';"
+"CREATE TABLE graduates (
+    ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL UNIQUE,
+    Age INTEGER,
+    Gender TEXT,
+    Points INTEGER,
+    Graduation TEXT
+);"
+"INSERT INTO graduates (ID, name, Age, Gender, Points) 
+SELECT * FROM students 
+WHERE name = 'Layal';"
+"UPDATE graduates
+SET Graduation = '08/09/2019'
+WHERE name = 'Layal';"
+"DELETE from students WHERE name = 'Layal';"
+
+
+
+
+
+
+
+
 
 
