72f63cabfd2ab5e5c52afca70a814f9899e8359e
linarawwas <linamkraw2@gmail.com>
2022-12-11 21:44:19 +0100

    Basic Queries

---
+++ 04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,17 @@
+SELECT * FROM students;
+SELECT * FROM students
+WHERE Age > 30;
+SELECT * FROM students
+where age=30 AND Gender='F';
+SELECT Points FROM students
+WHERE Name='Alex';
+INSERT INTO students
+VALUES (7,'Lina',23,'F',900);
+UPDATE students
+SET Points=500
+WHERE Name='Basma';
+UPDATE students
+set Points=100
+WHERE Name='Alex';
+
+
